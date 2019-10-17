8c5203fc18fbace8752779022a4685860cd61dc9
Keane Ferrnandes <31629150+keane-fernandes@users.noreply.github.com>
2019-10-17 17:14:29 +0200

    Create caesar.c

---
+++ pset2/caesar.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{   
+    
+    if (argc < 2 || argc > 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }      
+    string input = argv[1];
+    for (i = 0, n = strlen(input); i < n; i++);
+    {
+        printf("%c", input
+    }
+    // Print checks:
+    //printf("%i\n", inputCheck);
+    printf("%s\n", argv[1]);
+}
