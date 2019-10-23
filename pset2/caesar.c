184fd02349d0d7a5721d64f7a6e0c6138ac3e39a
Keane Ferrnandes <31629150+keane-fernandes@users.noreply.github.com>
2019-10-23 15:38:22 +0200

    Add files via upload

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -2,20 +2,59 @@
 #include <stdio.h>
 #include <string.h>
 
+// Program which ciphers text.
 int main(int argc, string argv[])
 {   
-    
-    if (argc < 2 || argc > 2)
+    // Check for number of arguments:
+    if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
-    }      
-    string input = argv[1];
-    for (i = 0, n = strlen(input); i < n; i++);
+    } 
+    
+    // Check that each character of string is a decimal digit:
+    string s = argv[1];
+    for (int i = 0, n = strlen(s); i < n; i++)
     {
-        printf("%c", input
+        if (s[i] < '0' || s[i] > '9')
+        {
+            printf("Usage: ./caesar key\n");
+            return 1;            
+        }
     }
+    
+    // Check the key falls within acceptable range:
+    int k = atoi(argv[1]);
+    if (k < 0 || k > 26)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;        
+    }
+    
+    // Ask user for input:
+    string plainText = get_string("plaintext: ");
+    printf("\n");
+    
+    string cipherText = plainText;
+    printf("Plain Text: %s\n", plainText);    
+    
+    for (int i = 0, n = strlen(cipherText); i < n; i++)
+    {
+        if (cipherText[i] >= 'a' && cipherText[i] <= 'z')
+        {
+            cipherText[i] = (((cipherText[i] - 96) + k) % 26) + 96;
+        }
+        else if (cipherText[i] >= 'A' && cipherText[i] <= 'Z')
+        {
+            cipherText[i] = (((cipherText[i] - 64) + k) % 26) + 64;
+        }
+        else
+        {
+            cipherText[i] = cipherText[i];
+        }
+    }
+    
+    
     // Print checks:
-    //printf("%i\n", inputCheck);
-    printf("%s\n", argv[1]);
+    printf("ciphertext: %s\n", cipherText);
 }
